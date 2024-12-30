7d591f6e7f8aba9fd2556adae8a73d6ad4c236aa
pigartpigaro <vichoirul@gmail.com>
2024-12-30 22:07:21 +0700

    revisi jurnal

--- src/pages/siasik/akuntansi/jurnal/registerjurnal/inpage/FormParam.vue
+++ src/pages/siasik/akuntansi/jurnal/registerjurnal/inpage/FormParam.vue
@@ -1,41 +1,19 @@
 <template>
   <div class="row">
     <div class="q-pa-sm">
-      <app-autocomplete
-        v-model="store.reqs.bulan"
-        label="Pilih Bulan"
-        autocomplete="nama"
-        option-label="nama"
-        option-value="value"
-        outlined
-        :source="store.bulans"
-      />
+      <app-autocomplete v-model="store.reqs.bulan" label="Pilih Bulan" autocomplete="nama" option-label="nama"
+        option-value="value" outlined :source="store.bulans" />
     </div>
     <div class="q-pa-sm">
-      <app-input
-        v-model="store.reqs.tahun"
-        label="Tahun"
-        outlined
-      />
+      <app-input v-model="store.reqs.tahun" label="Tahun" outlined />
     </div>
     <div class="q-pa-sm">
-      <app-btn
-        label="Ambil Data"
-        :disable="store.loading"
-        :loading="store.loading"
-        @click="ambilData()"
-      />
+      <app-btn label="Ambil Data" :disable="store.loading" :loading="store.loading" @click="ambilData()" />
     </div>
-    <!-- <div class="q-pa-sm">
-      <app-btn
-        label="Posting"
-        color="warning"
-        class="text-teal-9 text-weight-bold"
-        :disable="store.loading"
-        :loading="store.loading"
-        @click="store.posting()"
-      />
-    </div> -->
+    <div class="q-pa-sm">
+      <app-btn label="Posting" color="warning" class="text-teal-9 text-weight-bold" :disable="store.loading"
+        :loading="store.loading" @click="store.posting()" />
+    </div>
   </div>
 
   <!-- <dialog-jurnal
@@ -57,7 +35,7 @@ onMounted(() => {
 })
 
 // eslint-disable-next-line no-unused-vars
-function ambilData () {
+function ambilData() {
   store.getRegJurnal()
 }
 
