44c2873d5e9bd2470f1cb15b33b70954aac58559
pigartpigaro <vichoirul@gmail.com>
2025-01-12 16:35:58 +0700

    :rev page NPD LS

--- src/pages/siasik/transaksi/ls/npdls/PageNpdLS.vue
+++ src/pages/siasik/transaksi/ls/npdls/PageNpdLS.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="container q-pl-sm q-pr-sm">
     <div class="q-card q-mt-sm q-mt-ms">
       <q-card class="q-pa-xs">
@@ -11,14 +11,7 @@
     </div>
 
     <div class="q-card q-mt-sm q-mt-ms">
-      <q-tabs
-        v-model="tabdata"
-        dense
-        active-color="primary"
-        indicator-color="primary"
-        align="justify"
-        narrow-indicator
-      >
+      <q-tabs v-model="tabdata" dense active-color="primary" indicator-color="primary" align="justify" narrow-indicator>
         <q-tab name="forminput" label="Form NPD-LS" @click="reset()" />
         <q-tab name="data" label="Data NPD-LS" />
       </q-tabs>
@@ -49,7 +42,7 @@ const tabdata = ref('forminput')
 onBeforeUnmount(() => {
   reset()
 })
-function reset () {
+function reset() {
   store.resetFORM(tabdata)
   // carisrt.resetFORM(tabdata)
 }
@@ -58,6 +51,85 @@ function reset () {
 // })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped></style> -->
 
-</style>
+
+<template>
+  <q-page ref="pageRef" class="column full-height full-width" :class="!style.componentfull ? 'q-pa-md' : 'q-pa-xs'">
+    <div class="col-auto">
+      <PageHead v-if="!style.componentfull" :title="title" :subtitle="subtitle" :path="page.path"
+        @togle-draw="togleDraw()" />
+    </div>
+    <q-card flat class="col full-width full-height bg-grey-6"
+      :style="`max-height: ${!style.componentfull ? h - 60 : h + 40}px; overflow:hidden`">
+      <q-scroll-area :style="`height: ${!style.componentfull ? h - 95 : h + 40}px; max-width: 100%;`"
+        :thumb-style="thumbStyle" :bar-style="barStyle">
+        <router-view v-slot="{ Component, route }">
+          <transition :name="route.meta.transition || 'fade'">
+            <component :is="Component" transition />
+          </transition>
+        </router-view>
+      </q-scroll-area>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup>
+import { useStyledStore } from 'src/stores/app/styled'
+import PageHead from './PageHeadNpdls.vue'
+import { computed, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const style = useStyledStore()
+const pageRef = ref()
+const h = ref(0)
+const thumbStyle = ref({
+  right: '0px',
+  borderRadius: '5px',
+  backgroundColor: '#027be3',
+  width: '2px',
+  opacity: 0.75
+})
+const barStyle = ref({
+  right: '0px',
+  borderRadius: '9px',
+  backgroundColor: '#027be3',
+  width: '5px',
+  opacity: 0.2
+})
+
+const page = useRoute()
+const title = computed(() => {
+  if (page.path === '/siasik/ls/npdls/listnpd') {
+    return 'LIST NPD-LS'
+  }
+  // else if (page.path === '/akuntansi/jurnal/postingjurnal') {
+  //   return 'POSTING JURNAL'
+  // }
+  // else if (page.path === '/pendaftaran/umum/listmjkn') {
+  // //   return 'LIST MOBILE JKN'
+  // }
+  else {
+    return 'FORM NPD-LS'
+  }
+})
+
+const subtitle = computed(() => {
+  if (page.path === '/siasik/ls/npdls/listnpd') {
+    return 'List Data NPD-LS'
+  }
+  // else if (page.path === '/akuntansi/jurnal/postingjurnal') {
+  //   return 'List Jurnal yang Terposting'
+  // }
+  // // } else if (page.path === '/pendaftaran/umum/listmjkn') {
+  // //   return 'List Kunjungan dari m-JKN'
+  // }
+  else {
+    return 'Form Input Pengajuan NPD-LS'
+  }
+})
+
+onMounted(() => {
+  h.value = pageRef.value.$el.clientHeight
+})
+</script>
