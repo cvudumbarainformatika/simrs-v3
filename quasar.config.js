584de251c0aa8cf42923dbabbaa39a393629119c
duro06 <andi.meka.025@gmail.com>
2025-01-13 08:45:21 +0700

    rev config : nambah lang: id

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ export default defineConfig((ctx) => {
       // polyfillModulePreload: true,
       // distDir
 
-      extendViteConf(viteConf) {
+      extendViteConf (viteConf) {
         viteConf.build.chunkSizeWarningLimit = 5000
         // viteConf.build.rollupOptions = {
         //   output: {
@@ -110,7 +110,7 @@ export default defineConfig((ctx) => {
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
-
+      lang: 'id', // Quasar language pack ganti jadi bahas Indonesia
       // For special cases outside of where the auto-import strategy can have an impact
       // (like functional components as one of the examples),
       // you can manually specify Quasar components/directives to be available everywhere:
