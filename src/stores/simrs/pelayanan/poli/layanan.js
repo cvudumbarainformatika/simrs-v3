17a5b721db5947cd6d17af89cd3c389be32c4635
duro06 <andi.meka.025@gmail.com>
2025-01-24 10:02:44 +0700

    rev : mengembalikan diganosa

--- src/stores/simrs/pelayanan/poli/layanan.js
+++ src/stores/simrs/pelayanan/poli/layanan.js
@@ -8,8 +8,8 @@ import { notifErrVue, notifSuccess } from 'src/modules/utils'
 export const useLayananPoli = defineStore('layanan-poli', {
   state: () => ({
     tab: 'Tindakan Medik',
-    tabs: ['Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'],
-    // tabs: ['Diagnosa Medik', 'Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'], // diagnosa di hapus
+    // tabs: ['Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'],
+    tabs: ['Diagnosa Medik', 'Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'], // diagnosa di hapus
     // diagnosa
     searchdiagnosa: '',
     listDiagnosa: [],
@@ -371,8 +371,8 @@ export const useLayananPoli = defineStore('layanan-poli', {
       const tabbed = x ?? 'Tindakan Medik'
       return new Promise((resolve, reject) => {
         this.tab = tabbed
-        // this.tabs = ['Diagnosa Medik', 'Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'] // diagnosa medik di hapus
-        this.tabs = ['Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia']
+        this.tabs = ['Diagnosa Medik', 'Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia'] // diagnosa medik di hapus
+        // this.tabs = ['Tindakan Medik', 'Prosedur (Icd 9)', 'Diagnosa Keperawatan', 'Diagnosa Kebidanan', 'Laporan Tindakan', 'Pra Anestesia']
 
         this.searchdiagnosa = ''
         this.formdiagnosa = {
