c0e4c91602c46c08256fdbedc457b93001fcfeeb
pigartpigaro <vichoirul@gmail.com>
2025-03-21 15:27:32 +0700

    rev: LRA Jurnal

--- src/pages/siasik/akuntansi/laporanakuntansi/lralevel3/inpage/FormGetdata.vue
+++ src/pages/siasik/akuntansi/laporanakuntansi/lralevel3/inpage/FormGetdata.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="row full-width">
-    <div class="q-pa-sm" style="width:25%">
+  <div class="row justify-center full-width">
+    <!-- <div class="q-pa-sm" style="width:25%">
       <app-autocomplete
         v-model="refJenis"
         label="Pilih Jenis LRA"
@@ -18,68 +18,32 @@
           store.reqs.levelberapa = ''
         }"
       />
-    </div>
+    </div> -->
     <div class="q-pa-sm" style="width:25%">
-      <app-autocomplete
-        v-model="refData"
-        label="Pilih Rekening"
-        autocomplete="nama"
-        option-value="value"
-        option-label="nama"
-        outlined
-        :disable="store.loading || store.reqs.jenislra === 2"
-        :loading="store.loading"
-        :source="store.level"
-        :key="ambilData"
-        @update:model-value="(val)=>{
+      <app-autocomplete v-model="refData" label="Pilih Rekening" autocomplete="nama" option-value="value"
+        option-label="nama" outlined :disable="store.loading" :loading="store.loading" :source="store.level"
+        :key="ambilData" @update:model-value="(val) => {
           const valx = parseInt(val)
           store.reqs.levelberapa = valx
           console.log('ref', store.reqs.levelberapa)
-        }"
-      />
+        }" />
     </div>
     <div class="q-pa-sm" style="width:25%">
-      <app-input-date-human
-        :model="store.reqs.tgl"
-        label="dari tangal"
-        outlined
-        :disable="store.loading"
-        :loading="store.loading"
-        @db-model="tglDari"
-        @set-display="setDari"
-      />
+      <app-input-date-human :model="store.reqs.tgl" label="dari tangal" outlined :disable="store.loading"
+        :loading="store.loading" @db-model="tglDari" @set-display="setDari" />
     </div>
     <div class="q-pa-sm" style="width:25%">
-      <app-input-date-human
-        :model="store.reqs.tglx"
-        label="sampai tangal"
-        outlined
-        :disable="store.loading"
-        :loading="store.loading"
-        @db-model="tglSampai"
-        @set-display="setSampai"
-      />
+      <app-input-date-human :model="store.reqs.tglx" label="sampai tangal" outlined :disable="store.loading"
+        :loading="store.loading" @db-model="tglSampai" @set-display="setSampai" />
     </div>
   </div>
   <div class="row full-width justify-center">
     <div class="q-pa-sm">
-      <app-btn
-        label="Ambil Data"
-        :disable="store.loading"
-        :loading="store.loading"
-        @click="ambilData()"
-      />
+      <app-btn label="Ambil Data" :disable="store.loading" :loading="store.loading" @click="ambilData()" />
     </div>
     <div class="q-pa-sm">
-      <q-btn
-        icon="icon-mat-print"
-        color="orange"
-        round
-        size="sm"
-        :disable="store.loading"
-        :loading="store.loading"
-        @click="cetakData()"
-      >
+      <q-btn icon="icon-mat-print" color="orange" round size="sm" :disable="store.loading" :loading="store.loading"
+        @click="cetakData()">
         <q-tooltip class="bg-orange" :offset="[10, 10]">
           Cetak
         </q-tooltip>
@@ -94,27 +58,16 @@
       :before-finish="store.finishDownload"
       :name="'Buku Besar ' + store.reqs.tahun +'.xls'"
     > -->
-      <q-btn
-        icon="icon-mat-download"
-        color="green"
-        round
-        size="sm"
-        push
-        :disable="store.loading"
-        :loading="store.loading"
-        @click="store.exportExcel= !store.exportExcel"
-      >
+      <q-btn icon="icon-mat-download" color="green" round size="sm" push :disable="store.loading"
+        :loading="store.loading" @click="store.exportExcel = !store.exportExcel">
         <q-tooltip class="bg-green" :offset="[10, 10]">
           Export to Excel
         </q-tooltip>
       </q-btn>
-    <!-- </download-excel> -->
+      <!-- </download-excel> -->
     </div>
   </div>
-  <cetak-lra
-    v-model="store.dialogCetak"
-    :printlra="printlra"
-  />
+  <cetak-lra v-model="store.dialogCetak" :printlra="printlra" />
 </template>
 <script setup>
 import { useQuasar } from 'quasar'
@@ -129,23 +82,23 @@ const $q = useQuasar()
 const refJenis = ref('')
 const refData = ref('')
 
-function tglDari (val) {
+function tglDari(val) {
   store.setParameter('tgl', val)
 }
-function setDari (val) {
+function setDari(val) {
   store.display.dari = val
 }
-function tglSampai (val) {
+function tglSampai(val) {
   store.setParameter('tglx', val)
 }
-function setSampai (val) {
+function setSampai(val) {
   store.display.sampai = val
 }
-function ambilData () {
+function ambilData() {
   store.getDataLap()
 }
 const printlra = ref(null)
-function cetakData () {
+function cetakData() {
   store.dialogCetak = true
 }
 
@@ -163,7 +116,7 @@ function cetakData () {
 //     return store.kodejenis
 //   }
 // }
-function exportToExcel (tableId, filename) {
+function exportToExcel(tableId, filename) {
   // const el = document.getElementById(tableId)
   // const filenames = filename ? filename + '.xls' : 'KartuStokFarmasi.xls'
   // const columns = store.items
