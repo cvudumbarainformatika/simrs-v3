b5db73a38da0f7c82afef42234f61c2deb8e5cd5
duro06 <andi.meka.025@gmail.com>
2025-04-16 08:40:50 +0700

    rev : berita Acara stok opname

--- src/pages/simrs/farmasi/beritaacarastokopname/comp/BeritaAcaraPage.vue
+++ src/pages/simrs/farmasi/beritaacarastokopname/comp/BeritaAcaraPage.vue
@@ -2,19 +2,8 @@
   <div>
     <div class="row justify-end q-py-sm">
       <div class="col-auto q-mr-md">
-        <q-btn
-          ref="refPrint"
-          v-print="printObj"
-          unelevated
-          color="dark"
-          round
-          size="sm"
-          icon="icon-mat-print"
-        >
-          <q-tooltip
-            class="primary"
-            :offset="[10, 10]"
-          >
+        <q-btn ref="refPrint" v-print="printObj" unelevated color="dark" round size="sm" icon="icon-mat-print">
+          <q-tooltip class="primary" :offset="[10, 10]">
             Print
           </q-tooltip>
         </q-btn>
@@ -29,69 +18,74 @@
       </div>
       <div class="row justify-center q-mt-md f-18 text-weight-bold">
         BERITA ACARA STOK OPNAME
-      </div><div class="row justify-center q-mb-md q-mt-sm print-only">
+      </div>
+      <div class="row justify-center q-mb-md q-mt-sm print-only">
         Nomor : {{ store.form.no_ba }}
       </div>
       <div class="row justify-center items-center q-mb-md q-mt-sm print-hide">
         <div class="col-srink q-mr-sm">
           Nomor :
-        </div> <div class="col-shrink" style="width: 40%;">
-          <app-input
-            v-model="store.form.no_ba"
-            label="Nomor Surat"
-            outlined
-          />
+        </div>
+        <div class="col-shrink" style="width: 40%;">
+          <app-input v-model="store.form.no_ba" label="Nomor Surat" outlined />
         </div>
       </div>
       <div class="row">
         <div class="print-hide" style="width: 20%;">
-          <app-input-date-human
-            :model="store.periodeOpname.tgl_ba" label="Tanggal Berita Acara" outlined @set-display="(val)=>{
-              store.periodeOpname.tgl_ba=val
-            }"
-            @db-model="val=>{
-              store.form.tgl_ba=val
-            }"
-          />
+          <app-input-date-human :model="store.periodeOpname.tgl_ba" label="Tanggal Berita Acara" outlined @set-display="(val) => {
+            store.periodeOpname.tgl_ba = val
+          }" @db-model="val => {
+            store.form.tgl_ba = val
+          }" />
         </div>
       </div>
-      <p>Pada hari ini, {{ date.formatDate(store.form.tgl_ba, 'dddd') }}, tanggal {{ date.formatDate(store.form.tgl_ba, 'DD MMMM YYYY') }}, petugas farmasi yang disebutkan di bawah ini:</p>
+      <p>Pada hari ini, {{ date.formatDate(store.form.tgl_ba, 'dddd') }}, tanggal {{ date.formatDate(store.form.tgl_ba,
+        'DD MMMM YYYY') }}, petugas farmasi yang disebutkan di bawah ini:</p>
+      <div class="row print-hide justify-between">
+        <div class="col-6">
+          <app-autocomplete v-model="store.tempPeg" label="Pilih pegawai" :source="store.pegawaies" option-label="nama"
+            option-value="id" outlined clearable />
+        </div>
+        <div class="col-auto">
+          <app-btn label="Tambah" :disable="!store.tempPeg" @click="store.tambahPelaksana" />
+        </div>
+      </div>
       <!-- para pelaksana -->
-      <div v-for="(item,i) in store.form.pelaksanas" :key="item">
+      <div v-for="(item, i) in store.form.pelaksanas" :key="item">
         <div class="row">
           <div class="col-auto" style="width: 3%;">
-            {{ i+1 }}.
+            {{ i + 1 }}.
           </div>
           <div class="col-auto" style="width: 40%;">
-            {{ store.pelaksanas.find(f=>f.id===item)?.nama??'pelaksana tidak ditemukan' }}
+            {{store.pelaksanas.find(f => f.id === item)?.nama ?? 'pelaksana tidak ditemukan'}}
           </div>
           <div class="col-auto" style="width: 2%;">
             -
           </div>
           <div class="col-auto" style="width: 50%;">
-            {{ store.pelaksanas.find(f=>f.id===item)?.relasi_jabatan?.jabatan??'jabatan tidak ditemukan' }}
+            {{store.pelaksanas.find(f => f.id === item)?.relasi_jabatan?.jabatan ?? 'jabatan tidak ditemukan'}}
           </div>
           <div class="col-auto text-right print-hide" style="width: 5%;">
-            <q-btn
-              dense
-              flat
-              color="negative"
-              icon="icon-mat-delete_sweep"
-              @click="()=>{
-                const index=store.form.pelaksanas.findIndex(f=>f===item)
-                console.log('index',index);
+            <q-btn dense flat color="negative" icon="icon-mat-delete_sweep" @click="() => {
+              const index = store.form.pelaksanas.findIndex(f => f === item)
+              console.log('index', index);
 
-                if(index>=0) store.form.pelaksanas.splice(index,1)
-              }"
-            >
+              if (index >= 0) store.form.pelaksanas.splice(index, 1)
+            }">
               <q-tooltip>Hapus Pelaksana</q-tooltip>
             </q-btn>
           </div>
         </div>
       </div>
-
+      <div class="print-hide row">
+        <q-option-group v-model="store.gudangModel" :options="store.optionGudangs" color="primary" inline
+          @update:model-value="store.selectGudang" />
+      </div>
       <p> Telah melaksanakan kegiatan stok opname persediaan farmasi di ruangan sebagai berikut:</p>
-      <div class="row q-ml-md">
+      <div class="row q-ml-md text-weight-bold">
+        {{ store.gudangSelected?.nama }}
+      </div>
+      <!-- <div class="row q-ml-md">
         1. Gudang Kamar Obat
       </div>
       <div class="row q-ml-md">
@@ -108,7 +102,8 @@
       </div>
       <div class="row q-ml-md">
         6. Depo OK
-      </div>
+      </div> -->
+
       <p>
         Adapun hasil dari stok opname tersebut tercantum dalam lampiran.
       </p>
@@ -117,20 +112,14 @@
       </p>
       <div class="row q-mt-xl justify-end q-mr-lg print-hide">
         <div class="col-6">
-          <app-autocomplete
-            v-model="store.form.peg_id_pj_so"
-            label="Pilih Penanggung Jawab"
-            :source="store.pegawaies"
-            option-label="nama"
-            option-value="id"
-            outlined
-            clearable
-          />
+          <app-autocomplete v-model="store.form.peg_id_pj_so" label="Pilih Penanggung Jawab" :source="store.pegawaies"
+            option-label="nama" option-value="id" outlined clearable />
         </div>
       </div>
       <div class="row q-mt-xl justify-end q-mr-lg">
         <div class="col-4 text-center">
-          Probolinggo, {{ date.formatDate(store.form.tgl_selesai, 'dddd') }} {{ date.formatDate(store.form.tgl_selesai, 'DD MMMM YYYY') }}
+          Probolinggo, {{ date.formatDate(store.form.tgl_selesai, 'dddd') }} {{ date.formatDate(store.form.tgl_selesai,
+            'DD MMMM YYYY') }}
         </div>
       </div>
       <div class="row q-mt-sm q-mb-xl justify-between q-mx-lg">
@@ -138,7 +127,10 @@
           {{ store.KaUang?.relasi_jabatan?.jabatan }}
         </div>
         <div class="col-4 text-center">
-          {{ store.KaFarmasi?.relasi_jabatan?.jabatan?.toLowerCase()?.includes('farmasi')?store.KaFarmasi?.relasi_jabatan?.jabatan:store.KaFarmasi?.jabatan_tambahan?.jabatan }}
+          {{
+            store.KaFarmasi?.relasi_jabatan?.jabatan?.toLowerCase()?.includes('farmasi') ?
+              store.KaFarmasi?.relasi_jabatan?.jabatan : store.KaFarmasi?.jabatan_tambahan?.jabatan
+          }}
         </div>
         <div class="col-4 text-center">
           Penanggung Jawab
@@ -153,7 +145,7 @@
         </div>
 
         <div class="col-4 text-center">
-          {{ store.pegawaies.find(f=>f.id===store.form.peg_id_pj_so)?.nama??'Penanggung Jawab Belum dipilih' }}
+          {{store.pegawaies.find(f => f.id === store.form.peg_id_pj_so)?.nama ?? 'Penanggung Jawab Belum dipilih'}}
         </div>
       </div>
     </div>
@@ -161,9 +153,9 @@
     <!-- lampiran -->
     <div v-if="store.form?.tglopname">
       <div class="q-mt-xl q-mb-md " />
-      <div v-for="(gudang , i) in store.gudangs" :key="gudang" class="q-my-md">
+      <!-- <div v-for="(gudang, i) in store.gudangs" :key="gudang" class="q-my-md">
         <div class="row text-weight-bold">
-          Lampiran {{ i+1 }}
+          Lampiran {{ i + 1 }}
         </div>
         <div class="row q-mb-sm">
           {{ gudang?.nama }}
@@ -196,9 +188,9 @@
             </div>
           </div>
           <div v-for="(item, t) in gudang?.opname" :key="item">
-            <div class="row" :class="t%2===1?'bg-grey-4':'bg-grey-1'">
+            <div class="row" :class="t % 2 === 1 ? 'bg-grey-4' : 'bg-grey-1'">
               <div class="col-auto" style="width: 5%;">
-                {{ t+1 }}.
+                {{ t + 1 }}.
               </div>
               <div class="col-auto f-10" style="width: 10%;">
                 {{ item?.kd_obat }}
@@ -210,17 +202,84 @@
                 {{ item?.satuan_k }}
               </div>
               <div class="col-auto" style="width: 10%;">
-                {{ formatDouble( parseFloat(item?.jmlFisik??0),2) }}
+                {{ formatDouble(parseFloat(item?.jmlFisik ?? 0), 2) }}
               </div>
               <div class="col-auto" style="width: 10%;">
-                {{ formatDouble( parseFloat(item?.jmlOpname??0),2) }}
+                {{ formatDouble(parseFloat(item?.jmlOpname ?? 0), 2) }}
               </div>
-              <div class="col-auto" :class="(parseInt(item?.jmlFisik??0) - parseInt(item?.jmlOpname??0))!==0?'text-negative':''" style="width: 10%;">
-                {{ formatDouble( (parseFloat(item?.jmlFisik??0) - parseFloat(item?.jmlOpname??0)),2) }}
+              <div class="col-auto"
+                :class="(parseInt(item?.jmlFisik ?? 0) - parseInt(item?.jmlOpname ?? 0)) !== 0 ? 'text-negative' : ''"
+                style="width: 10%;">
+                {{ formatDouble((parseFloat(item?.jmlFisik ?? 0) - parseFloat(item?.jmlOpname ?? 0)), 2) }}
               </div>
               <div class="col-auto" style="width: 25%;">
                 {{ item?.keterangan }}
               </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <div class="row text-weight-bold">
+        Lampiran
+      </div>
+
+      <div class="row q-mb-sm">
+        {{ store.gudangSelected?.nama }}
+      </div>
+      <div v-if="store.gudangSelected?.opname?.length">
+        <div class="row bg-dark text-white text-weight-bold q-pa-xs">
+          <div class="col-auto" style="width: 5%;">
+            No
+          </div>
+          <div class="col-auto" style="width: 10%;">
+            Kode Obat
+          </div>
+          <div class="col-auto" style="width: 20%;">
+            Nama Obat
+          </div>
+          <div class="col-auto" style="width: 10%;">
+            Satuan
+          </div>
+          <div class="col-auto" style="width: 10%;">
+            Stok Fisik
+          </div>
+          <div class="col-auto" style="width: 10%;">
+            Stok Sistem
+          </div>
+          <div class="col-auto" style="width: 10%;">
+            Selisih Stok
+          </div>
+          <div class="col-auto" style="width: 25%;">
+            Keterangan
+          </div>
+        </div>
+        <div v-for="(item, t) in store.gudangSelected?.opname" :key="item">
+          <div class="row" :class="t % 2 === 1 ? 'bg-grey-4' : 'bg-grey-1'">
+            <div class="col-auto" style="width: 5%;">
+              {{ t + 1 }}.
+            </div>
+            <div class="col-auto f-10" style="width: 10%;">
+              {{ item?.kd_obat }}
+            </div>
+            <div class="col-auto" style="width: 20%;">
+              {{ item?.nama_obat }}
+            </div>
+            <div class="col-auto" style="width: 10%;">
+              {{ item?.satuan_k }}
+            </div>
+            <div class="col-auto" style="width: 10%;">
+              {{ formatDouble(parseFloat(item?.jmlFisik ?? 0), 2) }}
+            </div>
+            <div class="col-auto" style="width: 10%;">
+              {{ formatDouble(parseFloat(item?.jmlOpname ?? 0), 2) }}
+            </div>
+            <div class="col-auto"
+              :class="(parseInt(item?.jmlFisik ?? 0) - parseInt(item?.jmlOpname ?? 0)) !== 0 ? 'text-negative' : ''"
+              style="width: 10%;">
+              {{ formatDouble((parseFloat(item?.jmlFisik ?? 0) - parseFloat(item?.jmlOpname ?? 0)), 2) }}
+            </div>
+            <div class="col-auto" style="width: 25%;">
+              {{ item?.keterangan }}
             </div>
           </div>
         </div>
@@ -242,7 +301,7 @@ const printObj = {
 }
 </script>
 <style lang="scss" scoped>
-p{
+p {
   text-indent: 15px;
   padding-top: 10px;
   padding-bottom: 5px;
