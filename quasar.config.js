e498ef690af5c627445745c7636691ec9f3fd48e
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-04-18 23:09:11 +0700

    rev: config

--- quasar.config.js
+++ quasar.config.js
@@ -43,15 +43,20 @@ export default defineConfig((ctx) => {
       env: {
         // NODE_OPTIONS: '--max-old-space-size=20480',
         API: ctx?.dev
-          // ? 'http://192.168.150.111:3507'
-          ? 'http://localhost:8080'
-          : 'http://192.168.150.111:3507',
-        // : 'http://192.168.150.111:3501',
+          ? 'http://192.168.150.112:3501'
+          // ? 'http://localhost:8000'
+          // : 'http://192.168.150.111:3507',
+          : 'http://192.168.150.112:3501',
 
         WSHOST: ctx?.dev
-          // ? '192.168.20.37'
-          ? 'localhost'
-          : '192.168.150.111',
+          ? '192.168.150.112'
+          // ? 'localhost'
+          : '192.168.150.112',
+
+        PATHIMG: ctx?.dev
+          // ? 'http://localhost:8000/storage/'
+          ? 'http://192.168.150.111:3507'
+          : 'http://192.168.150.111:3507',
 
         ENABLE_MONITORING: ctx.dev, // Aktifkan hanya di development
         MONITORING_LOG: true // Matikan console log
