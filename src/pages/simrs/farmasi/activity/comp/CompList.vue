ad99b150adef71a221256e24cf90564b614e185a
duro06 <andi.meka.025@gmail.com>
2025-08-14 09:32:55 +0700

    rev : activiti jika desc bukan json

--- src/pages/simrs/farmasi/activity/comp/CompList.vue
+++ src/pages/simrs/farmasi/activity/comp/CompList.vue
@@ -12,49 +12,55 @@
       </template>
 
       <template #cell-description="{ row }">
-        <div class="row full-width text-weight-bold">
-          <div class="col-6">
-            Original
+        <template v-if="isJSON(row.description)">
+          <div class="row full-width text-weight-bold">
+            <div class="col-6">
+              Original
+            </div>
+            <div class="col-3">
+              Before
+            </div>
+            <div class="col-3">
+              After
+            </div>
           </div>
-          <div class="col-3">
-            Before
+          <div class="row full-width">
+            <div class=" col-4">
+              <template v-if="desc(row.description)?.original">
+                <div v-for="item in getKeys(desc(row.description)?.original)" :key="item" class="row">
+                  <div class="col-4">{{ item }}</div>
+                  <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
+                    desc(row.description)?.original[item]) : desc(row.description)?.original[item] }}</div>
+                </div>
+              </template>
+            </div>
+            <div class="col-4">
+              <template v-if="desc(row.description)?.before">
+                <div v-for="item in getKeys(desc(row.description)?.before)" :key="item" class="row">
+                  <div class="col-4">{{ item }}</div>
+                  <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
+                    desc(row.description)?.before[item]) : desc(row.description)?.before[item] }}</div>
+                </div>
+              </template>
+            </div>
+            <div class="col-4">
+              <template v-if="desc(row.description)?.after">
+                <div v-for="item in getKeys(desc(row.description)?.after)" :key="item" class="row">
+                  <div class="col-4">{{ item }}</div>
+                  <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
+                    desc(row.description)?.after[item]) : desc(row.description)?.after[item] }}</div>
+                </div>
+              </template>
+            </div>
           </div>
-          <div class="col-3">
-            After
+          <div class="row q-pt-md">
+            {{ row.source }}
           </div>
-        </div>
-        <div class="row full-width">
-          <div class=" col-4">
-            <template v-if="desc(row.description)?.original">
-              <div v-for="item in getKeys(desc(row.description)?.original)" :key="item" class="row">
-                <div class="col-4">{{ item }}</div>
-                <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
-                  desc(row.description)?.original[item]) : desc(row.description)?.original[item] }}</div>
-              </div>
-            </template>
-          </div>
-          <div class="col-4">
-            <template v-if="desc(row.description)?.before">
-              <div v-for="item in getKeys(desc(row.description)?.before)" :key="item" class="row">
-                <div class="col-4">{{ item }}</div>
-                <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
-                  desc(row.description)?.before[item]) : desc(row.description)?.before[item] }}</div>
-              </div>
-            </template>
-          </div>
-          <div class="col-4">
-            <template v-if="desc(row.description)?.after">
-              <div v-for="item in getKeys(desc(row.description)?.after)" :key="item" class="row">
-                <div class="col-4">{{ item }}</div>
-                <div class="col-8">{{ item == 'created_at' || item == 'updated_at' ? dateFull(
-                  desc(row.description)?.after[item]) : desc(row.description)?.after[item] }}</div>
-              </div>
-            </template>
-          </div>
-        </div>
-        <div class="row q-pt-md">
-          {{ row.source }}
-        </div>
+        </template>
+        <template v-else>
+          <div class="row full-width">{{ row.description }}</div>
+        </template>
+
       </template>
 
 
@@ -67,7 +73,24 @@ import { useLogActivityFarmasiStore } from 'src/stores/simrs/farmasi/activity/ac
 
 const store = useLogActivityFarmasiStore()
 
-function desc (val) { return JSON.parse(val) }
+function desc (val) {
+  if (isJSON(val)) {
+    return JSON.parse(val)
+  }
+  else return val
+}
 function getKeys (val) { return Object.keys(val) }
+function isJSON (str) {
+  if (typeof str !== "string") return false
+  str = str.trim()
+  if (!(str.startsWith("{") || str.startsWith("["))) return false
+
+  try {
+    JSON.parse(str)
+    return true
+  } catch {
+    return false
+  }
+}
 
 </script>
