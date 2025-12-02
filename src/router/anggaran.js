ce6d96f36743856e93e0f23a1357a55ea9f507d0
pigartpigaro <vichoirul@gmail.com>
2025-12-02 09:38:51 +0700

    wip: anggaran Pendapatan

---
+++ src/router/anggaran.js
@@ -0,0 +1,32 @@
+const anggaran = {
+  path: '/anggaran',
+  component: () => import('layouts/V2/IndexLayout.vue'),
+  meta: { requireAuth: true },
+  children: [
+    { path: '', name: 'anggaran', redirect: '/anggaran/penyusunan' },
+
+    {
+      path: '/anggaran/penyusunan',
+      // name: 'laporan.rekap.datapasien',
+      component: () => import('pages/siasik/anggaran/penyusunan/anggaranpendapatan/IndexPage.vue'),
+      children: [
+        { path: '', redirect: '/anggaran/penyusunan/anggaranpendapatan' },
+        {
+          path: '/anggaran/penyusunan/anggaranpendapatan',
+          name: 'anggaran.penyusunan.anggaranpendapatan',
+          component: () =>
+            import('src/pages/siasik/anggaran/penyusunan/anggaranpendapatan/IndexPage.vue')
+        },
+
+
+      ]
+    },
+
+    {
+      path: '/anggaran/notfound',
+      component: () => import('pages/MidlewareNotFound.vue')
+    }
+
+  ]
+}
+export default anggaran
