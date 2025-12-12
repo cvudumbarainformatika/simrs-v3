1b00ec620a8155a234a7baa16ae7b7d7225b9810
duro06 <andi.meka.025@gmail.com>
2025-12-12 14:37:31 +0700

    dikit

--- src/pages/simrs/kamaroperasi/pelayanan/complayout/LeftDrawer.vue
+++ src/pages/simrs/kamaroperasi/pelayanan/complayout/LeftDrawer.vue
@@ -1,54 +1,28 @@
 <template>
-  <q-scroll-area :style="`height: calc(100% - ${tinggiDetailPas * 2}px); margin-top: ${tinggiDetailPas}px; border-right: 1px solid #ddd`">
+  <q-scroll-area
+    :style="`height: calc(100% - ${tinggiDetailPas * 2}px); margin-top: ${tinggiDetailPas}px; border-right: 1px solid #ddd`">
     <q-separator />
-    <ListMenu
-      :key="menu"
-      :menus="menus"
-      :menu="menu"
-      @menu-click="(val) => emits('clickMenu', val)"
-    />
+    <ListMenu :key="menu" :menus="menus" :menu="menu" @menu-click="(val) => emits('clickMenu', val)" />
   </q-scroll-area>
 
-  <div
-    class="absolute-bottom"
-    :style="`height: ${tinggiDetailPas}px`"
-  >
+  <div class="absolute-bottom" :style="`height: ${tinggiDetailPas}px`">
     <!-- <SimulasiPageTwo :pasien="pasien" /> -->
   </div>
 
-  <div
-    class="absolute-top bg-dark text-white "
-    :style=" `height: ${tinggiDetailPas}px`"
-  >
+  <div class="absolute-top bg-dark text-white " :style="`height: ${tinggiDetailPas}px`">
     <div class="absolute-top-right">
       <div class="q-pa-sm">
-        <q-btn
-          outline
-          round
-          style="color: goldenrod;"
-          label="id"
-        />
+        <q-btn outline round style="color: goldenrod;" label="id" />
       </div>
     </div>
     <div class="absolute-top">
       <div class="q-pa-sm">
-        <q-badge
-          outline
-          color="orange"
-          :label="`${pasien?.sistembayar?.rs2 ?? '-'}`"
-        />
+        <q-badge outline color="orange" :label="`${pasien?.sistembayar?.rs2 ?? '-'}`" />
       </div>
     </div>
     <div class="absolute-bottom">
-      <div
-        class="q-pa-md cursor-pointer"
-        @click="emits('showProfile')"
-      >
-        <app-avatar-pasien
-          :key="pasien"
-          :pasien="pasien"
-          width="50px"
-        />
+      <div class="q-pa-sm cursor-pointer" @click="emits('showProfile')">
+        <app-avatar-pasien :key="pasien" :pasien="pasien" width="50px" />
         <div class="text-weight-bold f-12 q-mt-sm">
           {{ pasien?.kunjunganranap?.masterpasien?.rs2 ?? pasien?.kunjunganrajal?.masterpasien?.rs2 }}
         </div>
@@ -59,40 +33,23 @@
           {{ pasien ? pasien.rs1 : '-' }} || {{ pasien?.kunjunganranap?.rs2 ?? pasien?.kunjunganrajal?.rs2 }}
         </div>
         <div class="text-yellow text-italic f-10">
-          {{ pasien?.usia?? '-' }} Tahun || {{ pasien?.kunjunganranap?.masterpasien?.rs17 ?? pasien?.kunjunganrajal?.masterpasien?.kelamin }}
+          {{ pasien?.usia ?? '-' }} Tahun || {{ pasien?.kunjunganranap?.masterpasien?.rs17 ??
+            pasien?.kunjunganrajal?.masterpasien?.kelamin }}
         </div>
       </div>
       <q-bar>
         <q-space />
-        <q-btn
-          dense
-          flat
-          icon="icon-mat-attach_money"
-          class="gt-xs"
-          @click="bukaBill"
-        >
+        <q-btn dense flat icon="icon-mat-attach_money" class="gt-xs" @click="bukaBill">
           <q-tooltip class="bg-dark text-white">
             Billing Pasien
           </q-tooltip>
         </q-btn>
-        <q-btn
-          dense
-          flat
-          icon="icon-mat-menu_book"
-          class="gt-xs"
-          @click="emits('icare')"
-        >
+        <q-btn dense flat icon="icon-mat-menu_book" class="gt-xs" @click="emits('icare')">
           <q-tooltip class="bg-dark text-white">
             i-care
           </q-tooltip>
         </q-btn>
-        <q-btn
-          dense
-          flat
-          icon="icon-mat-history"
-          class="gt-xs"
-          @click="emits('historyPasien')"
-        >
+        <q-btn dense flat icon="icon-mat-history" class="gt-xs" @click="emits('historyPasien')">
           <q-tooltip class="bg-dark text-white">
             History Pasien
           </q-tooltip>
@@ -133,7 +90,7 @@ defineProps({
   }
 })
 const refBilling = ref(null)
-function bukaBill() {
+function bukaBill () {
   refBilling.value.openFaktur()
   // console.log('ref bill', refBilling.value)
   printRekap.value = true
