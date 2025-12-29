08a501842de587259ac0905935c263817b038510
pigartpigaro <vichoirul@gmail.com>
2025-12-29 09:58:00 +0700

    sementara

---
+++ src/pages/siasik/anggaran/penyusunan/pengusulan/FormData/comp/FormRincian.vue
@@ -0,0 +1,134 @@
+<template>
+  <q-form ref="formNpdLS" @submit="saveNpd">
+    <div class="col-12 col-md-12">
+      <div class="row">
+        <div class="col-6 q-pa-sm q-gutter-y-md">
+          <app-autocomplete label="Jenis Pengusulan" v-model="store.form.jenis" autocomplete="label"
+            option-value="value" option-label="label" outlined :disable="store.disabled" :source="pilihanJenis"
+            @update:model-value="updateJenis" />
+
+        </div>
+        <div class="col-6 q-pa-sm q-gutter-y-md">
+          <q-select v-model="store.form.kode" use-input outlined standout="bg-yellow-3" dense emit-value map-options
+            option-value="kode" input-debounce="300" label="Item Pengusulan" class="ellipsis-2-lines"
+            :options="optionsBarangs" clearable
+            :option-label="opt => opt?.kode ? `${opt.kode} - ${opt.nama}` : `${opt.nama}`" :disable="store.loadingSave"
+            :loading="store.loadingSave" @filter="filterFnBarang" @clear="store.setForm('kode', null)"
+            @update:model-value="updateBarangs">
+            <template #no-option>
+              <q-item>
+                <q-item-section class="text-grey">Data Tidak Ditemukan / Sudah Ada di List</q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 q-pa-sm q-gutter-y-md">
+          <app-input-simrs label="Volume" v-model="store.form.volume" :disable="store.disabled" outlined dense />
+
+
+        </div>
+        <div class="col-3 q-pa-sm q-gutter-y-md">
+          <app-input-simrs label="Harga" v-model="store.form.harga" :disable="store.disabled" outlined dense />
+        </div>
+        <div class="col-3 q-pa-sm q-gutter-y-md">
+          <app-autocomplete label="Jenis Pengusulan" v-model="store.form.jenis" autocomplete="label"
+            option-value="value" option-label="label" outlined :disable="store.disabled" :source="pilihanJenis"
+            @update:model-value="updateJenis" />
+
+        </div>
+      </div>
+      <div class="col-2">
+        <q-option-group class="q-col-gutter-md" type="checkbox" />
+      </div>
+    </div>
+  </q-form>
+</template>
+<script setup>
+import { notifErrVue } from 'src/modules/utils';
+import { dataBastFarmasiStore } from 'src/stores/siasik/transaksi/ls/newnpdls/bastfarmasi';
+import { formInputNpdlsStore } from 'src/stores/siasik/transaksi/ls/newnpdls/formnpdls';
+import { defineAsyncComponent, onMounted, ref } from 'vue';
+import { formattanpaRp } from 'src/modules/formatter'
+import { usePengusulanAnggaranStore } from 'src/stores/siasik/anggaran/penyusunan/pengusulan';
+// const FormInputPajak = defineAsyncComponent(() => import('./formpajak/FormPajak.vue'))
+
+
+const store = usePengusulanAnggaranStore()
+const carisrt = dataBastFarmasiStore()
+onMounted(() => {
+  console.log('store barang', store.dataBarangs)
+  store.getBarangs()
+})
+
+const pilihanJenis = [
+  { label: 'Barang Habis Pakai', value: 'Barang' },
+  { label: 'Barang Modal', value: 'Modal' },
+  { label: 'Jasa dan lain-lain', value: 'Jasa' },
+]
+
+function updateJenis(val) {
+  console.log('updatejenis', val)
+}
+
+const optionsBarangs = ref([])
+async function filterFnBarang(val, update) {
+  if (!val) {
+    update(() => {
+      optionsBarangs.value = store.dataBarangs
+    })
+    return
+  }
+
+  const needle = val.toLowerCase()
+
+  const localFiltered = store.dataBarangs.filter(v =>
+    v.kode?.toLowerCase().includes(needle) ||
+    v.nama?.toLowerCase().includes(needle)
+  )
+
+  if (localFiltered.length) {
+    update(() => {
+      optionsBarangs.value = localFiltered
+    })
+    return
+  }
+
+  let allData = []
+  let page = 1
+  let hasMore = true
+
+  while (hasMore) {
+    try {
+      const resp = await api.get(
+        'v1/barangs/index',
+        {
+          params: {
+            q: val,
+            per_page: 100,
+            page
+          }
+        }
+      )
+
+      const data = resp.data.data || []
+      allData.push(...data)
+      hasMore = resp.data.next_page_url !== null
+      page++
+    } catch (err) {
+      console.error(err)
+      hasMore = false
+    }
+  }
+
+  update(() => {
+    optionsBarangs.value = allData.map(a => ({
+      ...a,
+      label: `${a.kode} - ${a.nama}`,
+      value: a.kode
+    }))
+  })
+}
+
+</script>
