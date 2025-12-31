efda168919ddbf5740fcc0235d74b284a6d380ef
pigartpigaro <vichoirul@gmail.com>
2025-12-31 09:18:29 +0700

    rev: master Jasa

--- src/pages/siasik/master/master_jasa/comp/FormPage.vue
+++ src/pages/siasik/master/master_jasa/comp/FormPage.vue
@@ -3,24 +3,21 @@
     <div class="" style="">
       <app-input-simrs label="Nama" v-model="store.form.nama" :disable="store.disabled" outlined dense />
     </div>
-    <!-- <div class="" style="">
+    <div class="" style="">
 
-      <q-select v-model="store.form.kode50" use-input outlined standout="bg-yellow-3" dense emit-value map-options
-        option-value="kodeall3" input-debounce="300" label="Kode Rekening" class="ellipsis-2-lines" :options="options"
-        clearable :option-label="opt => opt?.kodeall3 ? `${opt.kodeall3} - ${opt.uraian}` : ''" :disable="store.loading"
-        :loading="store.loading" @filter="filterFn" @clear="store.setForm('kode50', null)" @update:model-value="(val) => {
-          const arr = store.optionrekening || []
-          const cari = arr.find(x => x.kodeall3 === val)
-          store.form.uraian50 = cari ? cari.uraian : ''
-          store.form.kodeall = cari ? cari.kodeall2 : ''
+      <q-select v-model="store.form.satuan" use-input outlined standout="bg-yellow-3" dense emit-value map-options
+        option-value="satuanBarang" input-debounce="300" label="Satuan" class="ellipsis-2-lines" :options="options"
+        clearable option-label="satuanBarang" :disable="store.loading" :loading="store.loading" @filter="filterFn"
+        @clear="store.setForm('satuan', null)" @update:model-value="(val) => {
+
         }">
         <template #no-option>
           <q-item>
             <q-item-section class="text-grey">Tidak ditemukan</q-item-section>
           </q-item>
         </template>
-</q-select>
-</div> -->
+      </q-select>
+    </div>
 
     <q-separator class="q-my-lg" />
     <div class="text-right">
@@ -66,71 +63,6 @@ onMounted(() => {
 
 })
 
-// async function filterFn(val, update) {
-//   // Jika kosong → tampilkan semua data awal (page 1)
-//   if (!val) {
-//     update(() => {
-//       options.value = store.akuns.map(a => ({
-//         ...a,
-//         label: `${a.kodeall3} - ${a.uraian}`,
-//         value: a.kodeall3
-//       }))
-//     })
-//     return
-//   }
-
-//   // Jika panjang key < 2 → jangan call API
-//   if (val.length < 2) {
-//     update(() => {
-//       options.value = []
-//     })
-//     return
-//   }
-
-//   // Mulai pencarian server
-//   let allData = []
-//   let page = 1
-//   let hasMore = true
-
-//   while (hasMore) {
-//     try {
-//       const resp = await api.get('v1/master/siasik/jasa/getrekening', {
-//         params: {
-//           q: val,
-//           per_page: 100,
-//           page: page
-//         }
-//       })
-
-//       const data = resp.data.data || []
-
-//       if (data.length > 0) {
-//         allData = [...allData, ...data]
-//         hasMore = resp.data.next_page_url !== null
-//         page++
-//       } else {
-//         hasMore = false
-//       }
-
-//     } catch (e) {
-//       console.error('Error load page:', e)
-//       hasMore = false
-//     }
-//   }
-
-//   // Update hasil pencarian
-//   update(() => {
-//     options.value = allData.map(a => ({
-//       ...a,
-//       label: `${a.kodeall3} - ${a.uraian}`,
-//       value: a.kodeall3
-//     }))
-
-//     // Simpan supaya next search bisa cepat
-//     store.optionrekening = allData
-//   })
-// }
-
 async function filterFn(val, update) {
   if (!val || val.length < 2) {
     update(() => {
@@ -140,7 +72,7 @@ async function filterFn(val, update) {
   }
 
   try {
-    const resp = await api.get('v1/master/siasik/jasa/getrekening', {
+    const resp = await api.get('v1/master/siasik/jasa/getsatuan', {
       params: {
         q: val,
         per_page: 20 // kecil saja
@@ -152,8 +84,9 @@ async function filterFn(val, update) {
     update(() => {
       options.value = data.map(a => ({
         ...a,
-        label: `${a.kodeall3} - ${a.uraian}`,
-        value: a.kodeall3
+        // label: `${a.kodeall3} - ${a.uraian}`,
+        label: a.satuanBarang,
+        value: a.satuanBarang
       }))
     })
 
