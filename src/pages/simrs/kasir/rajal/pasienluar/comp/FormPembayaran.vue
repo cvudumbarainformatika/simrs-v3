2ba199cb26823a8b723f80814bc6070e05650b4c
kentangkotak <ariezlnd69@gmail.com>
2026-01-22 15:02:54 +0700

    FIX ; KASIR RAWAT JALAN PASIEN LUAR

--- src/pages/simrs/kasir/rajal/pasienluar/comp/FormPembayaran.vue
+++ src/pages/simrs/kasir/rajal/pasienluar/comp/FormPembayaran.vue
@@ -1,18 +1,15 @@
 <template>
   <q-page class="q-pa-md bg-grey-3">
     <q-card class="q-pa-md full-height shadow-2 rounded-borders">
-
       <!-- Header Info Pasien + Billing -->
       <div class="row q-col-gutter-sm items-start">
-
         <!-- Detail Pasien (50%) -->
         <div class="col-12 col-md-6">
           <q-card flat bordered class="shadow-2 rounded-borders q-mb-sm">
             <div class="row no-wrap items-start q-pa-sm">
-
               <div class="col-auto flex flex-center q-pa-xs bg-red">
                 <app-avatar-pasien :key="pasien" :pasien="pasien" width="100px" height="100px"
-                  style="background-color: #f44336; border-radius: 50%;" />
+                  style="background-color: #f44336; border-radius: 50%" />
               </div>
 
               <!-- Detail Pasien -->
@@ -21,52 +18,69 @@
                   🧑‍⚕️ Detail Pasien
                 </q-card-section>
 
-                <q-separator style="width: 90%;" />
+                <q-separator style="width: 90%" />
 
-                <q-card-section class=" q-pa-none">
+                <q-card-section class="q-pa-none">
                   <div class="row q-col-gutter-md">
                     <!-- Kolom Kiri -->
                     <div class="col-6">
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">Nama</div>
-                        <div class="col">{{ pasien?.nama ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          Nama
+                        </div>
+                        <div class="col">{{ pasien?.nama ?? "-" }}</div>
                       </div>
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">No. RM</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          No. RM
+                        </div>
                         <div class="col row items-center">
-                          <div>{{ pasien?.norm ?? '-' }}</div>
-                          <q-badge class="q-ml-sm" color="primary" outline>{{ pasien?.kelamin ?? '-' }}</q-badge>
+                          <div>{{ pasien?.norm ?? "-" }}</div>
+                          <q-badge class="q-ml-sm" color="primary" outline>{{
+                            pasien?.kelamin ?? "-"
+                            }}</q-badge>
                         </div>
                       </div>
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">No. Reg</div>
-                        <div class="col">{{ pasien?.noreg ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          No. Reg
+                        </div>
+                        <div class="col">{{ pasien?.noreg ?? "-" }}</div>
                       </div>
                     </div>
 
                     <!-- Kolom Kanan -->
                     <div class="col-6">
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">Tgl Kunjungan</div>
-                        <div class="col">{{ dateFullFormat(pasien?.tgl_kunjungan) ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          Tgl Kunjungan
+                        </div>
+                        <div class="col">
+                          {{ dateFullFormat(pasien?.tgl_kunjungan) ?? "-" }}
+                        </div>
                       </div>
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">Usia</div>
-                        <div class="col">{{ pasien?.usia ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          Usia
+                        </div>
+                        <div class="col">{{ pasien?.usia ?? "-" }}</div>
                       </div>
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">Poli</div>
-                        <div class="col">{{ pasien?.poli ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          Poli
+                        </div>
+                        <div class="col">{{ pasien?.poli ?? "-" }}</div>
                       </div>
                       <div class="row rapat-row">
-                        <div class="col-4 text-weight-bold text-grey-8">DPJP</div>
-                        <div class="col">{{ pasien?.dokter ?? '-' }}</div>
+                        <div class="col-4 text-weight-bold text-grey-8">
+                          DPJP
+                        </div>
+                        <div class="col">{{ pasien?.dokter ?? "-" }}</div>
                       </div>
                     </div>
                   </div>
                 </q-card-section>
               </div>
-
             </div>
           </q-card>
         </div>
@@ -75,28 +89,32 @@
         <div class="col-12 col-md-6">
           <q-card flat bordered class="bg-white shadow-2 rounded-borders q-pa-sm full-height">
             <div class="row no-wrap items-stretch">
-
               <!-- Kolom QR -->
               <div class="col-auto q-pa-xs flex flex-center">
-
-                <div class="q-mb-xs"
-                  style="width: 100px; height: 110px; display: flex; justify-content: center; align-items: center;">
-                  <q-spinner-ios v-if="storepembayaran.loadingbayar || storepembayaran.loadingpembayaran"
-                    color=" primary" size="24px" />
+                <div class="q-mb-xs" style="
+                    width: 100px;
+                    height: 110px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                  ">
+                  <q-spinner-ios v-if="
+                    storepembayaran.loadingbayar ||
+                    storepembayaran.loadingpembayaran
+                  " color=" primary" size="24px" />
                   <vue-qrcode v-else :value="qrUrl" tag="svg" :options="{
                     errorCorrectionLevel: 'Q',
                     color: {
                       dark: '#000000',
                       light: '#ffffff',
                     },
-                    margin: 0
+                    margin: 0,
                   }" />
                 </div>
               </div>
 
               <!-- Kolom kanan (billing + VA + tombol) -->
               <div class="col column justify-between q-pl-sm">
-
                 <!-- Header Total Billing -->
                 <q-card-section class="q-pa-none q-pb-sm">
                   <div class="row items-center justify-between">
@@ -115,14 +133,14 @@
                 <!-- VA + Tombol Bayar -->
                 <div class="row items-center justify-between q-mt-sm">
                   <div class="text-caption text-grey-9 bg-grey-2 q-px-sm q-py-xs rounded-borders"
-                    style="font-family: monospace;" v-if="storepembayaran.loadingbuttonbill">
+                    style="font-family: monospace" v-if="storepembayaran.loadingbuttonbill">
                     <q-spinner-facebook color="blue" size="24px" />
                   </div>
                   <div class="text-caption text-grey-9 bg-grey-2 q-px-sm q-py-xs rounded-borders"
-                    style="font-family: monospace;" v-else>
+                    style="font-family: monospace" v-else>
                     VA: {{ storepembayaran.va }}
                   </div>
-                  <div class="row items-center no-wrap">
+                  <!-- <div class="row items-center no-wrap">
                     <q-btn v-for="item in btnbilling" :key="item.value" flat dense no-caps glossy
                       class="btn-badge q-mr-xs" @click="bukaBill">
                       <q-badge color="negative" class="q-pa-xs bill-anim">
@@ -130,19 +148,16 @@
                         <q-icon name="payment" size="22px" class="q-ml-xs bill-icon" />
                       </q-badge>
                     </q-btn>
-                  </div>
-
+                  </div> -->
                 </div>
               </div>
             </div>
           </q-card>
         </div>
-
       </div>
       <q-separator spaced />
       <!-- Content Split -->
       <div class="row q-col-gutter-md full-height">
-
         <!-- Form Pembayaran -->
         <div class="col-4">
           <form-pembayaran-detail :pasien="prop?.pasien" :jenislayanan="store.jenispembayaran" :billing="prop?.billing"
@@ -159,7 +174,7 @@
       </div>
     </q-card>
   </q-page>
-  <CetakRekapBilling ref="refBilling" v-model="printRekap" :pasien="pasien" style="z-index: 19000;"
+  <CetakRekapBilling ref="refBilling" v-model="printRekap" :pasien="pasien" style="z-index: 19000"
     @tutup="actPrintRekap" />
   <!-- <q-dialog v-model="dialogBilling">
     <q-card style="width: 1000px; max-width: 90vw;">
@@ -182,47 +197,52 @@
 </template>
 
 <script setup>
-import { dateFullFormat, formatRpDouble } from 'src/modules/formatter'
-import { useKasirRajalListKunjunganStore } from 'src/stores/simrs/kasir/rajal/kunjungan'
+import { dateFullFormat, formatRpDouble } from "src/modules/formatter";
+import { useKasirRajalListKunjunganStore } from "src/stores/simrs/kasir/rajal/kunjungan";
 
-import FormPembayaranDetail from './FormPembayaranDetail.vue'
-import DataPembayaran from './DataPembayaran.vue'
-import { usePembayaranKasirRajalStore } from 'src/stores/simrs/kasir/rajal/pembayaran'
-import { computed, defineAsyncComponent, markRaw, ref, shallowRef } from 'vue'
-import DataFlagingNonTunai from './DataFlagingNonTunai.vue'
-import CetakRekapBilling from '../../../../kasir/rajal/listkunjungan/comp/CetakRekapBilling.vue'
+import FormPembayaranDetail from "./FormPembayaranDetail.vue";
+import DataPembayaran from "./DataPembayaran.vue";
+import { usePembayaranKasirRajalStore } from "src/stores/simrs/kasir/rajal/pembayaran";
+import { computed, defineAsyncComponent, markRaw, ref, shallowRef } from "vue";
+import DataFlagingNonTunai from "./DataFlagingNonTunai.vue";
+import CetakRekapBilling from "../../../../kasir/rajal/listkunjungan/comp/CetakRekapBilling.vue";
 
-const store = useKasirRajalListKunjunganStore()
-const storepembayaran = usePembayaranKasirRajalStore()
+const store = useKasirRajalListKunjunganStore();
+const storepembayaran = usePembayaranKasirRajalStore();
 const qrUrl = computed(() => {
-  const qrvalue = storepembayaran.qrcode === '' ? '123456789' : storepembayaran.qrcode// noreg
+  const qrvalue =
+    storepembayaran.qrcode === "" ? "123456789" : storepembayaran.qrcode; // noreg
   // const dok = 'GENERAL-CONSENT.png'
   // const asal = 'GENERAL-CONSENT'
   // const enc = btoa(`${noreg}|${dok}|${asal}`)
-  return `${qrvalue}`
+  return `${qrvalue}`;
   // return `https://xenter.my.id/qr-document?noreg=${noreg}&dokumen=${dok}&asal=${asal}`
-})
+});
 
 const prop = defineProps({
   pasien: { type: Object, default: () => ({}) },
   billing: { type: Object, default: () => ({}) },
   kwitansikarcis: { type: Array, default: () => [] },
   kwitansinontunai: { type: Array, default: () => [] },
-  getKwitansiTerbayar: { type: Array, default: () => [] }
-})
+  getKwitansiTerbayar: { type: Array, default: () => [] },
+});
 
-console.log('prop', prop.getKwitansiTerbayar)
-const currentPage = shallowRef(null)
+// console.log("prop", prop.getKwitansiTerbayar);
+const currentPage = shallowRef(null);
 
-currentPage.value = CetakRekapBilling
-const judul = ref('')
+currentPage.value = CetakRekapBilling;
+const judul = ref("");
 const btnbilling = ref([
   {
-    label: 'Billing Rekap',
-    value: 'billingrekap',
-    page: markRaw(defineAsyncComponent(() =>
-      import('../../../../kasir/rajal/listkunjungan/comp/CetakRekapBilling.vue')
-    ))
+    label: "Billing Rekap",
+    value: "billingrekap",
+    page: markRaw(
+      defineAsyncComponent(() =>
+        import(
+          "../../../../kasir/rajal/listkunjungan/comp/CetakRekapBilling.vue"
+        )
+      )
+    ),
   },
   // {
   //   label: 'Billing Detail',
@@ -231,16 +251,16 @@ const btnbilling = ref([
   //     import('../../../../dokumen/comppoli/BillingPage.vue')
   //   )
   // }
-])
-const printRekap = ref(false)
-const refBilling = ref(null)
+]);
+const printRekap = ref(false);
+const refBilling = ref(null);
 function bukaBill() {
-  refBilling.value.openFaktur()
+  refBilling.value.openFaktur();
   // console.log('ref bill', refBilling.value)
-  printRekap.value = true
+  printRekap.value = true;
 }
 function actPrintRekap() {
-  printRekap.value = false
+  printRekap.value = false;
 }
 // function billingRekap(item, pasien) {
 //   pasienDipilih.value = pasien ?? {}     // hindari undefined
@@ -248,7 +268,6 @@ function actPrintRekap() {
 //   judul.value = item.label               // judul dialog
 //   dialogBilling.value = true
 // }
-
 </script>
 
 <style scoped>
