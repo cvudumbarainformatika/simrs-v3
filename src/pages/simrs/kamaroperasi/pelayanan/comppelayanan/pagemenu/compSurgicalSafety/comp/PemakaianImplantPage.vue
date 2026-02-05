cc769615604ee580ca5a14b4bf01cbc2d3ed2e0f
duro06 <andi.meka.025@gmail.com>
2026-02-05 09:59:42 +0700

    WIP : implant

--- src/pages/simrs/kamaroperasi/pelayanan/comppelayanan/pagemenu/compSurgicalSafety/comp/PemakaianImplantPage.vue
+++ src/pages/simrs/kamaroperasi/pelayanan/comppelayanan/pagemenu/compSurgicalSafety/comp/PemakaianImplantPage.vue
@@ -1,8 +1,29 @@
 <template>
-  halaman pemakaian implant
+  <div class="row bg-white">
+    <div class=" col fit">
+      <div class="full-height columm q-col-gutter-x-xs">
+        <div class="">
+          <Info :pasien="pasien" />
+          <q-separator class="q-my-sm" size="2px" />
+        </div>
+        <div class=" ">
+          <div class="q-px-sm">
+            <div class="row ">
+              <div class="text-bold f-14 q-mr-sm">Pemakaian Implant </div>
+            </div>
+            <q-separator class="q-my-sm" size="1px" />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
+import { defineAsyncComponent } from 'vue'
 
+
+const Info = defineAsyncComponent(() => import('./InfoPasienPage.vue'))
 const props = defineProps({
   pasien: {
     type: Object,
