8520e3a5118f77a09d8b32d1691a677f66e6b391
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2026-02-26 14:56:10 +0700

    rev: ---

--- src/stores/simrs/pelayanan/rehabmedik/asessment.js
+++ src/stores/simrs/pelayanan/rehabmedik/asessment.js
@@ -7,10 +7,10 @@ export const useRehabmedikAsessmentStore = defineStore('rehabmedik_asessment', {
     loading: false
   }),
   actions: {
-    setItem (payload) {
+    setItem(payload) {
       this.item = payload
     },
-    async getData (payload) {
+    async getData(payload) {
       this.loading = true
       try {
         const resp = await api.get(`v1/simrs/rehabmedik/asessment/get?noreg=${payload}`)
@@ -18,7 +18,7 @@ export const useRehabmedikAsessmentStore = defineStore('rehabmedik_asessment', {
           this.item = resp.data
         }
       } catch (error) {
-        console.log(error)
+        // console.log(error)
       } finally {
         this.loading = false
       }
