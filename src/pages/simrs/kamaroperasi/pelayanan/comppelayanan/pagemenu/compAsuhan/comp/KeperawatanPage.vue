5973284f4dd467c8d19df11d46fcde85ad75ef2b
duro06 <andi.meka.025@gmail.com>
2026-05-20 10:22:19 +0700

    Feat : simpan Asuhan keperawatan

--- src/pages/simrs/kamaroperasi/pelayanan/comppelayanan/pagemenu/compAsuhan/comp/KeperawatanPage.vue
+++ src/pages/simrs/kamaroperasi/pelayanan/comppelayanan/pagemenu/compAsuhan/comp/KeperawatanPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row full-height" style="height: 600px;">
+  <div class="row full-height" style="min-height: 650px; margin-bottom: 100px;">
     <div class="col full-height">
       <div class="row f-18 text-weight-bold q-pa-sm justify-center">
         PENGKAJIAN DAN DIAGNOSA KEPERAWATAN
@@ -24,15 +24,15 @@
       </div>
       <div class="row q-col-gutter-xs satu-tab  q-py-sm">
         <div class="col-6">
-          <app-input-date :model="store.form.pengkajian_jam" label="Mulai Jam" :type-date="false" dense outlined
+          <app-input-date :model="store.form.pengkajian_jam_mulai" label="Mulai Jam" :type-date="false" dense outlined
             @set-model="(val) => {
-              store.form.pengkajian_jam = val
+              store.form.pengkajian_jam_mulai = val
             }" />
         </div>
         <div class="col-6">
-          <app-input-date :model="store.form.pengkajian_jam" label="Selesai Jam" :type-date="false" dense outlined
-            @set-model="(val) => {
-              store.form.pengkajian_jam = val
+          <app-input-date :model="store.form.pengkajian_jam_selesai" label="Selesai Jam" :type-date="false" dense
+            outlined @set-model="(val) => {
+              store.form.pengkajian_jam_selesai = val
             }" />
         </div>
       </div>
@@ -193,7 +193,8 @@
           <div v-if="store.form.pengkajian_cuci_luka == 'Ya'" class="row items-center">
             <div class="col-2">Jenis Cairan :</div>
             <div class="col-9">
-              <q-input v-model="store.form.pengkajian_cairan_untuk_cuci_luka" autofocus color="primary" dense />
+              <q-input v-model="store.form.pengkajian_cairan_untuk_cuci_luka" autofocus color="primary" dense
+                maxlength="255" :rules="[val => !val || val.length <= 255 || 'Maksimal 255 karakter']" />
             </div>
           </div>
         </div>
@@ -211,11 +212,13 @@
           <div v-if="store.form.pengkajian_obat_penutup_luka == 'Ya'" class="row items-center">
             <div class="col-2">Jenis :</div>
             <div class="col-9">
-              <q-input v-model="store.form.pengkajian_jenis_obat_penutup_luka" autofocus color="primary" dense />
+              <q-input v-model="store.form.pengkajian_jenis_obat_penutup_luka" autofocus color="primary" dense
+                maxlength="255" :rules="[val => !val || val.length <= 255 || 'Maksimal 255 karakter']" />
             </div>
             <div class="col-2">Jenis Balutan</div>
             <div class="col-9">
-              <q-input v-model="store.form.pengkajian_jenis_balutan_penutup_luka" color="primary" dense />
+              <q-input v-model="store.form.pengkajian_jenis_balutan_penutup_luka" color="primary" dense maxlength="255"
+                :rules="[val => !val || val.length <= 255 || 'Maksimal 255 karakter']" />
             </div>
           </div>
         </div>
@@ -224,12 +227,12 @@
       <div class="row f-18 text-weight-bold q-pa-sm justify-center">
         LUARAN
       </div>
-      <div class="row q-col-gutter-sm items-center q-pa-sm">
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
         <div class="col-6">luaran utama</div>
         <div class="col-6"><q-option-group v-model="store.form.luaran_utama" :options="store.optionLuarans"
             color="primary" inline dense type="checkbox" size="sm" /></div>
       </div>
-      <div class="row q-col-gutter-sm items-center q-pa-sm">
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
         <div class="col-6">Setelah dilakukan tindakan keperawatan selama 3x24 jam, maka tingkat infeksi menurun dengan
           kriteria hasil</div>
         <div class="col-6"><q-option-group v-model="store.form.luaran_hasil" :options="store.optionHasils"
@@ -238,19 +241,180 @@
       <div class="row f-18 text-weight-bold q-pa-sm justify-center">
         INTERVENSI DAN IMPLEMENTASI
       </div>
-      <div class="row q-col-gutter-sm items-center q-pa-sm">
-        <div class="col-3 text-weight-bold">luaran utama</div>
-        <div class="col-6"><q-option-group v-model="store.form.luaran_utama" :options="store.optionLuarans"
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
+        <div class="col-3 text-weight-bold">Intervensi utama</div>
+        <div class="col-6"><q-option-group v-model="store.form.intervensi_utama" :options="store.optionIntervensiUtamas"
             color="primary" inline dense type="checkbox" size="sm" /></div>
+      </div>
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
+        <div class="col-3 text-weight-bold">Intervensi pendukung</div>
+        <div class="col-6"><q-option-group v-model="store.form.intervensi_pendukung"
+            :options="store.optionIntervensiPendukungs" color="primary" inline dense type="checkbox" size="sm" /></div>
+      </div>
+      <div class="row f-14 q-pa-sm q-ml-lg">
+        IMPLEMENTASI :
+      </div>
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
+        <div class="col-3 text-weight-bold text-italic">Obseravasi</div>
+        <div class="col-6"><q-option-group v-model="store.form.implementasi_observasi" :options="store.optionObservasis"
+            color="primary" inline dense type="checkbox" size="sm" /></div>
+      </div>
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
+        <div class="col-3 text-weight-bold text-italic">Terupetik</div>
+        <div class="col-6"><q-option-group v-model="store.form.implementasi_terupetik" :options="store.optionTerupetiks"
+            color="primary" inline dense type="checkbox" size="sm" /></div>
+      </div>
+      <div class="satu-tab row q-col-gutter-sm items-center q-pa-sm">
+        <div class="col-3 text-weight-bold text-italic">Kolaborasi</div>
+        <div class="col-6"><q-option-group v-model="store.form.implementasi_kolaborasi"
+            :options="store.optionKolaborasis" color="primary" inline dense type="checkbox" size="sm" /></div>
       </div>
       <div class="row f-18 text-weight-bold q-pa-sm justify-center">
         EVALUASI
+      </div>
+      <div class="satu-tab row q-col-gutter-xs ">
+        <div class="col-6">
+          <app-input-date-human label="Tanggal" outlined :model="store.display.tanggal_eval" @db-model="(val) => {
+            store.form.eval_tanggal = val
+          }" @set-display="(val) => {
+            store.display.tanggal_eval = val
+          }" />
+        </div>
+        <div class="col-6">
+          <app-input-date :model="store.form.eval_jam" label="Jam" :type-date="false" dense outlined @set-model="(val) => {
+            store.form.eval_jam = val
+          }" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs ">
+        <div class="col-6">
+          <app-input v-model="store.form.eval_subjective" label="S" outlined />
+        </div>
+        <div class="col-6">
+          <app-input v-model="store.form.eval_objective" label="O" outlined />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">Kerusakan Jaringan</div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_kerusakan_jaringan" :options="store.ynOptions" color="primary" inline
+            dense size="sm" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">Kemerahan</div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_kemerahan" :options="store.ynOptions" color="primary" inline dense
+            size="sm" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">Oedem daerah incici</div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_oedem_daerah_incici" :options="store.ynOptions" color="primary"
+            inline dense size="sm" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">Haematon</div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_haematon" :options="store.ynOptions" color="primary" inline dense
+            size="sm" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">Cairan Drainase</div>
+        <div class="col-9">
+          <div class="row">
+            <q-option-group v-model="store.form.eval_cairan_drainase" :options="store.ynOptions" color="primary" inline
+              dense size="sm"
+              @update:model-value="(val) => { setNullIf(val, 'eval_jenis_cariran_drainase', 'Tidak') }" />
+          </div>
+          <div v-if="store.form.eval_cairan_drainase == 'Ya'" class="row items-center">
+            <div class="col-1">Jenis</div>
+            <div class="col-11">
+              <q-input v-model="store.form.eval_jenis_cariran_drainase" autofocus color="primary" dense maxlength="255"
+                :rules="[val => !val || val.length <= 255 || 'Maksimal 255 karakter']" />
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-sm items-center">
+        <div class="col-4">
+          <div class="row items-center no-wrap">
+            <div class="col-auto q-mr-xs" style="width: 20%;">Tensi : </div>
+            <div class="col-auto" style="width: 30%;"><app-input v-model="store.form.eval_td_sis" label="Sistole"
+                outlined /> </div>
+            <div class="col-auto">/</div>
+            <div class="col-auto" style="width: 30%;"><app-input v-model="store.form.eval_td_dia" label="Diastole"
+                outlined />
+            </div>
+            <div class="col-auto">mmHg</div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row items-center no-wrap">
+            <div class="col-auto q-mr-xs" style="width: 20%;">Suhu : </div>
+            <div class="col-auto" style="width: 60%;"><app-input v-model="store.form.eval_suhu" label="Suhu" outlined />
+            </div>
+            <div class="col-auto"></div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row items-center no-wrap">
+            <div class="col-auto q-mr-xs" style="width: 20%;">RR : </div>
+            <div class="col-auto" style="width: 60%;"><app-input v-model="store.form.eval_rr" label="RR" outlined />
+            </div>
+            <div class="col-auto">x/menit</div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row items-center no-wrap">
+            <div class="col-auto q-mr-xs" style="width: 20%;">HR : </div>
+            <div class="col-auto" style="width: 60%;"><app-input v-model="store.form.eval_hr" label="HR" outlined />
+            </div>
+            <div class="col-auto">x/menit</div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="row items-center no-wrap">
+            <div class="col-auto q-mr-xs" style="width: 20%;">SpO<sub>2</sub> : </div>
+            <div class="col-auto" style="width: 60%;">
+              <q-input v-model="store.form.eval_spo2" label="label" outlined dense>
+                <template #label>
+                  SpO<sub>2</sub>
+                </template>
+              </q-input>
+            </div>
+            <div class="col-auto">%</div>
+          </div>
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">A: Resiko Infeksi</div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_a_resiko_infeksi" :options="store.optionResikoInfeksis"
+            color="primary" inline dense size="sm" />
+        </div>
+      </div>
+      <div class="satu-tab row q-col-gutter-xs q-my-xs  ">
+        <div class="col-3">P : </div>
+        <div class="col-9">
+          <q-option-group v-model="store.form.eval_p" :options="store.optionPs" color="primary" inline dense
+            size="sm" />
+        </div>
+      </div>
+      <div class="row justify-end q-pa-xl">
+        <q-btn label="Simpan" color="primary" icon="save" rounded no-caps glossy :loading="store.loading"
+          :disable="store.loading" @click="store.simpan(pasien)" />
       </div>
     </div>
   </div>
 </template>
 <script setup>
 import { useAsuhanKeperawatanStore } from 'src/stores/simrs/kamaroperasi/asuhan/asuhanKeperawatan'
+import { onMounted } from 'vue'
 
 const props = defineProps({
   pasien: {
@@ -261,15 +425,20 @@ const props = defineProps({
 const store = useAsuhanKeperawatanStore()
 function ambilTidakanOperasi (val) {
   const data = val?.length > 0 ? val?.map(v => v?.mastertindakanoperasi?.rs2).join(', ') : 'Belum ada Tindakan Operasi '
-  console.log(data)
+  // console.log(data)
   return data
 }
 function setNullIf (value, keyToSet, condition) {
   // console.log('val', value, 'key', keyToSet, 'con', condition)
   if (value == condition) {
-    store.formPengkajian[keyToSet] = null
+    store.form[keyToSet] = null
   }
 }
+onMounted(() => {
+  // console.log('on mounted')
+
+  store.getData(props.pasien)
+})
 </script>
 <style lang="scss" scoped>
 .satu-tab {
--- src/stores/simrs/kamaroperasi/asuhan/asuhanKeperawatan.js
+++ src/stores/simrs/kamaroperasi/asuhan/asuhanKeperawatan.js
@@ -1,4 +1,5 @@
 import { acceptHMRUpdate, defineStore } from "pinia"
+import { api } from "src/boot/axios"
 
 export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
   state: () => ({
@@ -23,8 +24,8 @@ export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
       luaran_utama: [],
       luaran_hasil: [],
       // intervensi dan imlementasi
-      inverntensi_utama: [],
-      inverntensi_pendukung: [],
+      intervensi_utama: [],
+      intervensi_pendukung: [],
       implementasi_observasi: [],
       implementasi_terupetik: [],
       implementasi_kolaborasi: [],
@@ -35,9 +36,9 @@ export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
       eval_objective: null,
       eval_kerusakan_jaringan: null,
       eval_kemerahan: null,
-      eval_oedem_daeran_incici: null,
+      eval_oedem_daerah_incici: null,
       eval_haematon: null,
-      eval_cariran_drainase: null,
+      eval_cairan_drainase: null,
       eval_jenis_cariran_drainase: null,
       eval_td_sis: null,
       eval_td_dia: null,
@@ -47,7 +48,6 @@ export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
       eval_spo2: null,
       eval_a_resiko_infeksi: null,
       eval_p: null,
-
     },
     display: {
       tanggal: '',
@@ -103,11 +103,31 @@ export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
     ],
 
     // intervensi dan imlementasi
-    optionIntervensiUtama: [
-      { label: 'Tingkat Infeksi ( L.14137)', value: 'Tingkat Infeksi ( L.14137)' },
-      { label: 'Integritas kulit dan jaringan ( L.14125 )', value: 'Integritas kulit dan jaringan ( L.14125 )' },
-      { label: 'Kontrol Resiko ( L.14128 )', value: 'Kontrol Resiko ( L.14128 )' },
+    optionIntervensiUtamas: [
+      { label: 'Pencegahan infeksi ( I.14539 )', value: 'Pencegahan infeksi ( I.14539 )' },
     ],
+    optionIntervensiPendukungs: [
+      { label: 'Management lingkungan ( I.14514 )', value: 'Management lingkungan ( I.14514 )' },
+      { label: 'Perawatan area insisi ( I.14558 )', value: 'Perawatan area insisi ( I.14558 )' },
+    ],
+    optionObservasis: [
+      { label: 'Monitor lokasi insisi ( kemerahan, bengkak, atau tanda-tanda infeksi )', value: 'Monitor lokasi insisi ( kemerahan, bengkak, atau tanda-tanda infeksi )' },
+      { label: 'Monitor dan mempertahankan sterilisasi lingkungan operasi selama perioperatif', value: 'Monitor dan mempertahankan sterilisasi lingkungan operasi selama perioperatif' },
+    ],
+    optionTerupetiks: [
+      { label: 'Mengatur meja steril instrumen dan linen.', value: 'Mengatur meja steril instrumen dan linen.' },
+      { label: 'Menyiapkan dan mempertahankan instrumen dalam keadaan steril selama perioperatif.', value: 'Menyiapkan dan mempertahankan instrumen dalam keadaan steril selama perioperatif.' },
+      { label: 'Melakukan scrubing, glowing, gloving, dan draping dengan tenik antiseptik.', value: 'Melakukan scrubing, glowing, gloving, dan draping dengan tenik antiseptik.' },
+      { label: 'Membersihkan area operasi dengan antiseptic dari area dalam menuju area luar.', value: 'Membersihkan area operasi dengan antiseptic dari area dalam menuju area luar.' },
+      { label: 'Membatasi jumlah personil didalam OK.', value: 'Membatasi jumlah personil didalam OK.' },
+      { label: 'Mengatur suhu kamar operasi.', value: 'Mengatur suhu kamar operasi.' },
+      { label: 'Menerapkan prinsip teknik aseptic selama perioperative.', value: 'Menerapkan prinsip teknik aseptic selama perioperative.' },
+      { label: 'Melakukan pernutupan luka insisi dengan teknik aseptik.', value: 'Melakukan pernutupan luka insisi dengan teknik aseptik.' },
+    ],
+    optionKolaborasis: [
+      { label: 'Kolaborasi dengan operator untuk memastikan pembrian profilaksis.', value: 'Kolaborasi dengan operator untuk memastikan pembrian profilaksis.' },
+    ],
+    // Evaluasi
     optionResikoInfeksis: [
       { label: 'Tidak terjadi', value: 'Tidak terjadi' },
       { label: 'Terjadi', value: 'Terjadi' },
@@ -117,7 +137,99 @@ export const useAsuhanKeperawatanStore = defineStore('asuhan_keperawatan', {
       { label: 'Tingkat infeksi menurun', value: 'Tingkat infeksi menurun' },
     ],
   }),
-  actions: {}
+  actions: {
+    resetForm (pasien) {
+      console.log('resetForm')
+      this.form = {
+        noreg: pasien?.noreg,
+        norm: pasien?.norm,
+        nota: pasien?.rs2,
+
+        pengkajian_faktor_resiko: [],
+        pengkajian_posisi_canul_intra_vena: [],
+        pengkajian_jenis_operasi: null,
+        pengkajian_disinfeksi_area_operasi: null,
+        pengkajian_irigasi_luka: null,
+        pengkajian_cairan_irigasi_luka: null,
+        pengkajian_cuci_luka: null,
+        pengkajian_cairan_untuk_cuci_luka: null,
+        pengkajian_obat_penutup_luka: null,
+        pengkajian_jenis_obat_penutup_luka: null,
+        pengkajian_jenis_balutan_penutup_luka: null,
+        pengkajian_urine_katheter: null,
+        pengkajian_td_sis: null,
+        pengkajian_td_dia: null,
+        pengkajian_status_pernafasan: null,
+        // luaran
+        luaran_utama: [],
+        luaran_hasil: [],
+        // intervensi dan imlementasi
+        intervensi_utama: [],
+        intervensi_pendukung: [],
+        implementasi_observasi: [],
+        implementasi_terupetik: [],
+        implementasi_kolaborasi: [],
+        // evaluasi
+        eval_tanggal: null,
+        eval_jam: null,
+        eval_subjective: null,
+        eval_objective: null,
+        eval_kerusakan_jaringan: null,
+        eval_kemerahan: null,
+        eval_oedem_daerah_incici: null,
+        eval_haematon: null,
+        eval_cairan_drainase: null,
+        eval_jenis_cariran_drainase: null,
+        eval_td_sis: null,
+        eval_td_dia: null,
+        eval_suhu: null,
+        eval_rr: null,
+        eval_hr: null,
+        eval_spo2: null,
+        eval_a_resiko_infeksi: null,
+        eval_p: null,
+      }
+    },
+    async getData (pasien) {
+      try {
+        const params = {
+          params: {
+            noreg: pasien?.noreg,
+            norm: pasien?.norm,
+            nota: pasien?.rs2,
+          }
+        }
+        const resp = await api.get('v1/simrs/penunjang/ok/asuhan/keperawatan/getdata', params)
+        // console.log('resp get data', resp)
+        if (resp?.status === 200) {
+          const data = resp.data?.data
+          if (!!data) {
+            this.form = resp.data?.data
+            delete this.form.created_at
+            delete this.form.updated_at
+
+          } else {
+            this.resetForm(pasien)
+          }
+        }
+      } catch (e) { } finally { }
+    },
+    async simpan (pasien) {
+      // console.log('pasien', pasien, this.form)
+      this.loading = true
+      try {
+        const resp = await api.post('v1/simrs/penunjang/ok/asuhan/keperawatan/simpan', this.form)
+        console.log('resp simpan', resp)
+        if (resp?.status === 200) {
+          notifSuccess(resp)
+          this.getData(pasien)
+        }
+      } catch (e) { } finally {
+        this.loading = false
+      }
+
+    }
+  }
 })
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useAsuhanKeperawatanStore, import.meta.hot))
