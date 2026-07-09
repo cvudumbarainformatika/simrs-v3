d8ec1e93f9f0726d2b20ed84e8b1db6458e3311f
duro06 <andi.meka.025@gmail.com>
2026-07-09 10:32:34 +0700

    dokumen prabedah

--- src/pages/simrs/dokumen/erm/poli/AsesmenPraAnestesia.vue
+++ src/pages/simrs/dokumen/erm/poli/AsesmenPraAnestesia.vue
@@ -458,7 +458,8 @@
             </div>
 
             <div style="height: 150px; width: 150px;" class="column flex-center">
-              <figure v-if="pasien?.rs19 === '1'" class="qrcode full-width">
+              <!-- <figure v-if="pasien?.rs19 === '1'" class="qrcode full-width"> -->
+              <figure v-if="store.resultPraAnastesi?.length > 0" class="qrcode full-width">
                 <vue-qrcode :value="qrUrl" tag="svg" :options="{
                   errorCorrectionLevel: 'Q',
                   color: {
@@ -543,8 +544,9 @@ onMounted(async () => {
 const qrUrl = computed(() => {
   const noreg = props?.pasien?.noreg
   const dok = 'DOKUMEN PRA-ANESTESI.png'
-  const asal = 'RAWAT JALAN'
-  const enc = btoa(`${noreg}|${dok}|${asal}`)
+  const asal = 'PENUNJANG'
+  const petugas = dc?.kdpegsimrs ?? null
+  const enc = btoa(`${noreg}|${dok}|${asal}|${petugas}`)
   return `https://rsud.probolinggokota.go.id/dokumen-simrs/legalitas/${enc}`
   // return `https://xenter.my.id/qr-document?noreg=${noreg}&dokumen=${dok}&asal=${asal}`
 })
